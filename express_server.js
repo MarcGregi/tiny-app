0d6447a2877c58eca7dcff8ee6a43ddf0c5e2504
marcgregi <marc@hypedigital.co>
2019-01-15 23:05:30 +0000

    Updating the Generate Random String Function

--- express_server.js
+++ express_server.js
@@ -1,10 +1,4 @@
-
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs")
-
+// function to generate random 6 digit short url
 function generateRandomString() {
 let totalCharcters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let newString = '';
@@ -17,6 +11,13 @@ let newString = '';
 
 generateRandomString();
 
+
+
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs")
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -57,8 +58,8 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 
